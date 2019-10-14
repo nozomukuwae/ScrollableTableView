114913adf0aedc8d27f5722839ad9b2dac1de9c4
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-10-14 16:29:51 +0900

    Initial Commit

---
+++ ScrollableTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollableTableView
+//
+//  Created by 桑江 望 on 2019/10/14.
+//  Copyright © 2019 桑江 望. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
